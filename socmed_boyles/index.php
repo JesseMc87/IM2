28c79d1728e43f04a796c02507bc6ea1527f3704
Boyles, Prince Niño L. <94779042+JesseMc87@users.noreply.github.com>
2025-01-21 16:08:00 +0800

    Add files via upload

---
+++ socmed_boyles/index.php
@@ -0,0 +1,8 @@
+
+        <?php
+        include 'views/header.php';
+        include 'views/sample.php';
+        include 'views/sample.php';
+        include 'view/footer.php';
+        ?>
+    
---
+++ socmed_boyles/views/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Socmed</title>
+        <link href="../res/mystyle.css" rel="sylesheet" type="text/css"/>
+    </head>
+    <body>
